9530c419f6fe4dc118638a33aa465887b8041d2b
zalaznik <zc@sem-wolf.org>
2020-07-21 21:17:19 +0300

    implementation of the dictionary where the sentences are kept, first version - mock data

---
+++ meta_data/dict_of_sentences.py
@@ -0,0 +1,11 @@
+
+file_list = ["greeting", "nice_sentences", "cities"]
+
+sentences_dict = {
+    (0, 1): "hello big and nice world",
+    (1, 14): "to be or not to be",
+    (2, 23): "paris"
+}
+
+# Mock data till I'll implementation the read from the files
+# in the func : init_sentences_dict()
