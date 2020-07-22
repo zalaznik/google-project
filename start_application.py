d9d74c8400074c8d49747b43fb91305a2bf74430
zalaznik <zc@sem-wolf.org>
2020-07-22 12:22:53 +0300

    add the main function in which the flow of the application statrs

---
+++ start_application.py
@@ -0,0 +1,14 @@
+from .find_complete_sentences_online import init_system, get_best_k_completions
+
+
+if __name__ == "__main__":
+    print("starting...\n")
+    init_system()
+    print("finished\n")
+    while True:
+        input_ = input("please enter...\n")
+        while input_[-1] != '#':
+            # validate_input()
+            completions = get_best_k_completions(input_)
+            print(f"completions: {completions}\n{input_}")
+            input_ += input()
