9f333a67c930e024f3c6459fb04fe8f686378b62
zalaznik <zc@sem-wolf.org>
2020-07-29 03:17:49 +0300

    change search method to support the case of remove or add a letter and prevent duplications

--- code/meta_data/trie_of_substrings.py
+++ code/meta_data/trie_of_substrings.py
@@ -39,7 +39,7 @@ class Trie:
 
     def normal_search(self, node, string):
 
-        # the brother not exist
+        # the brother is not exist
         if node is None:
             return [], None, 0
 
@@ -56,15 +56,40 @@ class Trie:
 # need to add the search for the case of add a letter or remove a letter
 # now this func is works for the normal case and for the case of change a letter
     def search(self, sub_string):
+
+        # first searching for the simple case
         complete_list, father, traveled_length = self.normal_search(self.root, sub_string)
 
         while father and len(complete_list) < 5:
 
+            # case of exchange a letter
             for child in father.children:
                 current_complete_list, _, _ = self.normal_search(child, sub_string[traveled_length+1:])
 
-                if current_complete_list:
-                    complete_list += current_complete_list
+                for item in current_complete_list:
+                    if item not in complete_list:
+                        complete_list.append(item)
+
+                if len(complete_list) >= 5:
+                    return complete_list[:5]
+
+            # case of add a letter
+            current_complete_list, _, _ = self.normal_search(father, sub_string[traveled_length + 1:])
+
+            for item in current_complete_list:
+                if item not in complete_list:
+                    complete_list.append(item)
+
+            if len(complete_list) >= 5:
+                return complete_list[:5]
+
+            # case of remove a letter
+            for child in father.children:
+                current_complete_list, _, _ = self.normal_search(child, sub_string[traveled_length:])
+
+                for item in current_complete_list:
+                    if item not in complete_list:
+                        complete_list.append(item)
 
                 if len(complete_list) >= 5:
                     return complete_list[:5]
